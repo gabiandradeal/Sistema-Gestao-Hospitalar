38b15fcd9d3c9e8321cd405ca1393bfba16b4623
Horlanlacerda <horlansilva56@gmail.com>
2026-03-12 18:26:45 -0300

    feat(node): implementa a estrutura da BSTNode com getters, setters, isEmpty e toString

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -1,18 +1,16 @@
 package BST;
 
-public interface BST<T> {
+public interface BST<T extends Comparable <T>> {
 
     public boolean isEmpty();
     public int height();
-    public boolean search(T element);
+    public BSTNode<T> search(T element);
 
     public void insert(T element);
     public void remove(T element);
 
-    public T maximum();
-    public T minimum();
-
-    
+    public BSTNode<T> maximum();
+    public BSTNode<T> minimum();
 
     public T[] preOrder();
     public T[] Order();
