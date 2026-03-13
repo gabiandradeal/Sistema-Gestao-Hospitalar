da32e683d3fb71b48082dfd6df91dffb937b02ee
georiSamuel <georissamuelgouveia@gmail.com>
2026-03-12 23:53:18 -0300

    feat: adicionando classe Paciente e Enum de nível de emergência

--- src/model/NivelUrgencia.java
+++ src/model/NivelUrgencia.java
@@ -1,4 +1,12 @@
 package model;
 
 public enum NivelUrgencia {
+
+    // Segundo o Protocolo de Manchester
+
+    AZUL,     // Não urgente
+    VERDE,    // Pouco urgente
+    AMARELO,  // Urgente
+    LARANJA,  // Muito urgente
+    VERMELHO  // Emergência (Risco de vida)
 }
--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,4 +1,89 @@
 package model;
 
-public class Paciente {
+import java.util.Arrays;
+
+public class Paciente implements Comparable<Paciente> {
+    private String nome;
+    private String cpf; // Nosso ID
+    private NivelUrgencia urgencia;
+    private String[] sintomas;
+
+    // ==========================================
+    // <--------- GETTERS E SETTERS --------->
+    // ==========================================
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ClassificacaoRisco getUrgencia() {
+        return urgencia;
+    }
+    public void setUrgencia(ClassificacaoRisco urgencia) {
+        this.urgencia = urgencia;
+    }
+
+    public String[] getSintomas() {
+        return sintomas;
+    }
+    public void setSintomas(String[] sintomas) {
+        this.sintomas = sintomas;
+    }
+
+
+    // ========================================================
+    // <---------MÉTODOS PARA AS ESTRUTURAS DE DADOS --------->
+    // ========================================================
+
+    /**
+     * 🌳 PARA A ÁRVORE AVL
+     * Agora a Árvore será percorrida e balanceada com base no CPF.
+     */
+    @Override
+    public int compareTo(Paciente outroPaciente) {
+        return this.cpf.compareTo(outroPaciente.getCpf());
+    }
+
+    /*
+    /*
+     * 🔗 PARA A TABELA HASH (CHAINING)
+     * Garante que não haverá duplicatas de CPF na mesma lista encadeada.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return Objects.equals(cpf, paciente.cpf);
+    }
+
+    /*
+     * 🔗 PARA A TABELA HASH
+     * Gera o índice numérico (bucket) baseado no CPF.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    @Override
+    public String toString() {
+        // Arrays.toString() converte o array de forma limpa para impressão: [Febre, Tosse, Dor de Cabeça]
+        return "Paciente [" +
+                "CPF='" + cpf + '\'' +
+                ", Nome='" + nome + '\'' +
+                ", Urgência=" + urgencia +
+                ", Sintomas=" + Arrays.toString(sintomas) +
+                ']';
+    }
 }
+
